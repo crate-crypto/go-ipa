88710be83982379dea1548b42edc27d923273433
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-10-13 20:22:29 +0100

    [common]
    - remove appendBytes
    - remove now outdated comment

--- common/transcript.go
+++ common/transcript.go
@@ -11,7 +11,6 @@ import (
 /// See: Fiat-Shamir
 /// XXX: ideally, this should also contain labels, however this is not included in the python implementation
 // and we first want this different API to pass without modifying the tests.
-// TODO Feedback from gballet remove the buffer and store the SHa256 object
 type Transcript struct {
 	state hash.Hash
 }
@@ -33,8 +32,9 @@ func NewTranscript(label string) *Transcript {
 // the state
 func (t *Transcript) AppendScalar(scalar *bls.Fr) {
 	tmpBytes := bls.FrTo32(scalar)
-	t.appendBytes(tmpBytes[:])
+	t.state.Write(tmpBytes[:])
 }
+
 func (t *Transcript) AppendScalars(scalars ...*bls.Fr) {
 	for _, idx := range scalars {
 		t.AppendScalar(idx)
@@ -46,25 +46,13 @@ func (t *Transcript) AppendScalars(scalars ...*bls.Fr) {
 // Compresses the G1 Point into a 32 byte slice, then appends it to
 // the state
 func (t *Transcript) AppendPoint(point *bls.G1Point) {
-	// XXX: Ideally, we just add the compressed bytes into
-	// the transcript
-	// However, so that tests do not fail, we
-	// sha256 hash the compressed point
-	// tmpBytes := bls.ToCompressedG1(point)
-	//
-	//
-	tmpBytes := sha256.Sum256(bls.ToCompressedG1(point))
-	t.appendBytes(tmpBytes[:])
+	t.state.Write(bls.ToCompressedG1(point))
 }
+
 func (t *Transcript) AppendPoints(points ...*bls.G1Point) {
 	for _, point := range points {
 		t.AppendPoint(point)
 	}
-}
-
-// Appends Bytes to the transcript
-func (t *Transcript) appendBytes(b []byte) {
-	t.state.Write(b)
 }
 
 // Computes a challenge based off of the state of the transcript
@@ -72,10 +60,18 @@ func (t *Transcript) appendBytes(b []byte) {
 // Hash the transcript state, then reduce the hash modulo the size of the
 // scalar field
 //
-// XXX: Reduction to a field element, should probably be in the bls library and not here
-// with an api that just takes a slice and reduces it to an Fr
+// Note that calling the transcript twice, will yield two different challenges
 func (t *Transcript) ChallengeScalar() bls.Fr {
 	var tmp bls.Fr
 	bls.ReduceBytesToFr(&tmp, t.state.Sum(nil))
+
+	// Clear the state
+	t.state.Reset()
+
+	// Add the new challenge to the state
+	// Which "summarises" the previous state before we cleared it
+	t.AppendScalar(&tmp)
+
+	// Return the new challenge
 	return tmp
 }
