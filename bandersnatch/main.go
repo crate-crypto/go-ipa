81153a5468d4d9c4dfe5796b078e02bb9799ccf9
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-10-20 22:42:59 +0100

    add bandersnatch curve

---
+++ bandersnatch/main.go
@@ -0,0 +1,13 @@
+package main
+
+// export PATH=${PATH}:`go env GOPATH`/bin
+func main() {
+	// p is the basefield of bandersnatch/ jubjub
+	// fp, _ := field.NewField("fp", "Element", "52435875175126190479447740508185965837690552500527637822603658699938581184513")
+	// err := generator.GenerateFF(fp, "./fp")
+	// if err != nil {
+	// 	fmt.Printf("\n%s\n", err.Error())
+	// 	os.Exit(-1)
+	// }
+
+}
