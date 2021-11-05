b491efa9b8ada6919b9adeba422e733097ec8cdd
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-11-05 21:13:05 +0000

    Add method to test Serialize and Deserialization of a point

--- bandersnatch/point_test.go
+++ bandersnatch/point_test.go
@@ -12,3 +12,13 @@ func TestElementBytes(t *testing.T) {
 		panic("point should have prime order")
 	}
 }
+func TestSerDer(t *testing.T) {
+
+	gen := GetEdwardsCurve().Base
+	byts := gen.Bytes()
+	var p = &PointAffine{}
+	p.SetBytes(byts[:])
+	if !p.Equal(&gen) {
+		panic("bug in serialisaion ")
+	}
+}
