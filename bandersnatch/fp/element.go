bb5c76ff150ed6baa9b4e1bcc5a84ffe38cfdb85
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-05-18 15:30:02 +0100

    [Endian]
    - Values were not being put in little endian

--- bandersnatch/fp/element.go
+++ bandersnatch/fp/element.go
@@ -684,10 +684,10 @@ func (z *Element) Bytes() (res [Limbs * 8]byte) {
 // of z as a little-endian byte array.
 func (z *Element) BytesLE() (res [Limbs * 8]byte) {
 	_z := z.ToRegular()
-	binary.LittleEndian.PutUint64(res[24:32], _z[0])
-	binary.LittleEndian.PutUint64(res[16:24], _z[1])
-	binary.LittleEndian.PutUint64(res[8:16], _z[2])
-	binary.LittleEndian.PutUint64(res[0:8], _z[3])
+	binary.LittleEndian.PutUint64(res[24:32], _z[3])
+	binary.LittleEndian.PutUint64(res[16:24], _z[2])
+	binary.LittleEndian.PutUint64(res[8:16], _z[1])
+	binary.LittleEndian.PutUint64(res[0:8], _z[0])
 
 	return
 }
