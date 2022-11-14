8b02dbdb652b2073153302e07ee4cd353615a390
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-11-14 18:23:00 +0000

    Add comments

--- bandersnatch/fp/fp.go
+++ bandersnatch/fp/fp.go
@@ -6,27 +6,36 @@ import (
 	basefield "github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
+// A field element representing the basefield of the
+// bandersnatch curve
 type Element = basefield.Element
 
+// The number of limbs needed to represent this field element
 const Limbs = basefield.Limbs
 
+// The number 1 in montgomery form
 func One() Element {
 	return basefield.One()
 }
+
+// The number 0 in montgomery form
 func Zero() Element {
 	return basefield.Element{}
 }
 
+// The number `-1` in montgomery form
 func MinusOne() Element {
 	m_one := One()
 	m_one.Neg(&m_one)
 	return m_one
 }
 
+// Batch invert multiple field elements using the montgomery trick
 func BatchInvert(a []Element) []Element {
 	return basefield.BatchInvert(a)
 }
 
+// Serialise a field element in little-endian form
 func SerialiseLe(z Element) (res [Limbs * 8]byte) {
 	_z := z.ToRegular()
 	binary.BigEndian.PutUint64(res[24:32], _z[0])
