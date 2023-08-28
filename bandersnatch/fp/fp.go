8c5daf7319a6997cdfc44ef16f7401bd0f6a2e64
Ignacio Hagopian <jsign.uy@gmail.com>
2023-08-28 12:37:54 -0300

    fix comments
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- bandersnatch/fp/fp.go
+++ bandersnatch/fp/fp.go
@@ -4,40 +4,40 @@ import (
 	basefield "github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
-// A field element representing the basefield of the
-// bandersnatch curve.
+// Element is a field element representing the basefield of the curve.
 type Element = basefield.Element
 
-// The number of limbs needed to represent this field element.
+// Limbs is the number of 64-bit words needed to represent the field element.
 const Limbs = basefield.Limbs
 
-// The number 1 in montgomery form.
+// One returns the field element 1.
 func One() Element {
 	return basefield.One()
 }
 
-// The number -1 in montgomery form.
+// Zero returns the field element 0.
 func Zero() Element {
 	return basefield.Element{}
 }
 
-// One returns 1 (in montgommery form)
+// MinusOne returns the field element -1.
 func MinusOne() Element {
 	m_one := One()
 	m_one.Neg(&m_one)
 	return m_one
 }
 
+// MulBy5 multiplies a field element by 5.
 func MulBy5(a *Element) {
 	basefield.MulBy5(a)
 }
 
-// Batch invert multiple field elements using the montgomery trick.
+// BatchInvert computes the inverse of a slice of field elements.
 func BatchInvert(a []Element) []Element {
 	return basefield.BatchInvert(a)
 }
 
-// Serialise a field element in little-endian form.
+// BytesLE returns the little-endian byte representation of a field element.
 func BytesLE(a Element) []byte {
 	var result [basefield.Bytes]byte
 	basefield.LittleEndian.PutElement(&result, a)
